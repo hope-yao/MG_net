f9e9a10ef3ef3f5e13715afc257600bcd937df9c
hope-yao <hope-yao@asu.edu>
2018-10-22 01:07:22 -0700

    elast tf forward is correct, but training won't converge. thermal tf hasn't tested yet.

--- new_code_tf/thermal_2phase_2D_tf.py
+++ new_code_tf/thermal_2phase_2D_tf.py
@@ -37,77 +37,21 @@ class Jacobi_block():
         result['final'] = result['u_hist'][-1]
         return result
 
-def get_w_matrix(coef_dict):
-    E, mu = coef_dict['E'], coef_dict['mu']
-    cost_coef = E/16./(1-mu**2)
-    wxx = cost_coef * np.asarray([
-        [-4*(1 - mu / 3.),        16 * mu / 3.,             -4*(1 - mu / 3.)],
-        [-8*(1 + mu / 3.),       32. * (1 - mu / 3.),       -8*(1 + mu / 3.)],
-        [-4*(1 - mu / 3.),        16 * mu / 3.,             -4*(1 - mu / 3.)],
-    ])
-
-    wxy = wyx = cost_coef * np.asarray([
-        [-2 * (mu + 1),        0,             2 * (mu + 1)],
-        [0,                        0,                  0],
-        [2 * (mu + 1),        0,             -2 * (mu + 1)],
-    ])
-
-    wyy = cost_coef * np.asarray([
-        [-4 * (1 - mu / 3.),         -8 * (1 + mu / 3.),        -4 * (1 - mu / 3.)],
-        [16 * mu / 3.,               32. * (1 - mu / 3.),        16 * mu / 3.],
-        [-4 * (1 - mu / 3.),         -8 * (1 + mu / 3.),        -4 * (1 - mu / 3.)],
-    ])
-    return wxx, wxy, wyx, wyy
-
-def np_get_D_matrix_elast(coef_dict, mode='symm'):
-    # convolution with symmetric padding at boundary
-    d_matrix_xx_val, d_matrix_yy_val = coef_dict['wxx'][1,1], coef_dict['wyy'][1,1]
-    d_matrix_xx = d_matrix_xx_val*np.ones((num_node,num_node))
-    d_matrix_yy = d_matrix_yy_val*np.ones((num_node,num_node))
-    d_matrix = np.stack([d_matrix_xx, d_matrix_yy], -1)
-    d_matrix[0,:] /=2
-    d_matrix[-1,:] /=2
-    d_matrix[:,0] /=2
-    d_matrix[:,-1] /=2
-
-    return d_matrix
-
-
 def load_data_elem():
     '''loading data obtained from FEA simulation'''
-    # linear elasticity, all steel, Yfix
-    data = sio.loadmat('/home/hope-yao/Documents/MG_net/data/new_elasticity/crack.mat')
-    ux = data['d_x'].reshape(65,65).transpose((1,0)) #* 1e10
-    uy = data['d_y'].reshape(65,65).transpose((1,0)) #* 1e10
-    u_img = np.concatenate([np.expand_dims(ux, 2), np.expand_dims(uy, 2)], 2)
-    fx = data['f_x'].reshape(65,65).transpose((1,0))
-    fy = data['f_y'].reshape(65,65).transpose((1,0))
-    f_img = -1. * np.concatenate([np.expand_dims(fx, 2), np.expand_dims(fy, 2)], 2)
+    # data = sio.loadmat('./data/heat_transfer_1phase/matrix.mat')
+    # f = data['matrix'][0][0][1]
+    # A = data['matrix'][0][0][0]
 
+    # NEW MULTI CIRCLE CASE
+    data = sio.loadmat('./data/new_heat_transfer_multi_inclusion/0circle.mat')
     A = data['K']
-    # f = np.asarray(zip(data['f_x'], data['f_y'])).flatten()
-    f = np.asarray([var for tup in zip(data['f_x'], data['f_y']) for var in tup])
-
-    if 0:
-        # check FEA data is correct
-        u = np.asarray([var for tup in zip(data['d_x'], data['d_y']) for var in tup])
-        f_pred = np.matmul(A, u)
-        import matplotlib.pyplot as plt
-        f_pred_img = np.asarray([f_pred[2 * i] for i in range(65 ** 2)]).reshape(65, 65).transpose((1, 0))
-        plt.imshow(f_pred_img)
-        plt.show()
-    # get reference jacobi data
-    #ref_res = reference_jacobi_solver(A, f)
-
-    coef = {
-        'E': 20e10,  # 200e9,
-        'mu': 0.25,
-    }
-
-    coef['wxx'], coef['wxy'], coef['wyx'], coef['wyy'] = get_w_matrix(coef)
-    coef['d_matrix'] = np_get_D_matrix_elast(coef, mode='symm')
-
-    return u_img, f_img, coef
+    f = data['f']
+    u = np.linalg.solve(A, f)
+    u_img = u.reshape(1, 66, 66, 1).transpose((0, 2, 1, 3))
+    f_img = f.reshape(1, 66, 66, 1).transpose((0, 2, 1, 3))
+    conductivity = np.float32(16.)
+    return u_img, f_img, conductivity
 
 if __name__ == "__main__":
     import os
