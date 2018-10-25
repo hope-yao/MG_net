a23989dcb9d3cd8d193cdb5053e2baa19bcf34ba
jshanson <joseph.s.hanson@asu.edu>
2018-10-25 07:12:18 -0700

    Importing sparse matlab matrix, converting to an array of arrays and solving

--- MAE598_Project/2D_conduction_CG_py.py
+++ MAE598_Project/2D_conduction_CG_py.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy.io as sio
+from timeit import default_timer as timer
+from scipy.sparse import csr_matrix
 
 def conjgrad(A, b, tol, x):
     n = len(A)
@@ -21,20 +23,21 @@ def conjgrad(A, b, tol, x):
     return x
 
 if __name__ == '__main__':
-    #data1 = sio.loadmat('./data/K_Forceboundary_nodes4x4.mat')
-    #data2 = sio.loadmat('./data/f_forceboundary_nodes4x4.mat')
-    #data3 = sio.loadmat('./data/x0_nodes4x4.mat')
-    #AA = data1['K_Forceboundary_nodes4x4']
-    #Ab = data2['f_forceboundary_nodes4x4']
-    #xx = data3['x0_nodes4x4']
+    data1 = sio.loadmat('./data/100x100/K_Forceboundary_nodes100x100.mat')
+    data2 = sio.loadmat('./data/100x100/f_forceboundary_nodes100x100.mat')
+    data3 = sio.loadmat('./data/100x100/x0_nodes100x100.mat')
+    A = data1['K_Forceboundary_nodes100x100']
+    A = A.toarray()
+    b = data2['f_forceboundary_nodes100x100']
+    x = data3['x0_nodes100x100']
 
-    # Toy matrix A = 2x2
+    # Toy Test Matrix A = 2x2
     #A = np.array([[4., 1.],[1., 3.]])
     #b = np.array([[1.], [2.]])
 
-    # Toy matrix A = 8x8
-    b = np.array([[1], [1], [1], [1], [1], [1], [1], [1]])
-    A = np.array([[6, 0, 1, 2, 0, 0, 2, 1],
+    # Toy Test Matrix A = 8x8
+    bb = np.array([[1], [1], [1], [1], [1], [1], [1], [1]])
+    AA = np.array([[6, 0, 1, 2, 0, 0, 2, 1],
                   [0, 5, 1, 1, 0, 0, 3, 0],
                   [1, 1, 6, 1, 2, 0, 1, 2],
                   [2, 1, 1, 7, 1, 2, 1, 1],
@@ -42,9 +45,16 @@ if __name__ == '__main__':
                   [0, 0, 0, 2, 0, 4, 1, 0],
                   [2, 3, 1, 1, 2, 1, 5, 1],
                   [1, 0, 2, 1, 1, 0, 1, 3]])
-    x = np.array([[0], [0], [0], [0], [0], [0], [0], [0]])
+    #x = np.array([[0], [0], [0], [0], [0], [0], [0], [0]])
     tol = 1e-5
+
+    start = timer()
+
     x = conjgrad(A, b, tol, x)
-    x_test = np.linalg.solve(A, b)
+
+    end = timer()
+    print(end - start) # Time in seconds
+
+    # x_test = np.linalg.solve(A, b)
 
 
