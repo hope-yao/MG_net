8ddb64d0c57f2bfe60512a7957f8d153a8b24bf1
jshanson <joseph.s.hanson@asu.edu>
2018-10-29 23:08:48 -0700

    Formating

--- MAE598_Project/2D_conduction_CG_py.py
+++ MAE598_Project/2D_conduction_CG_py.py
@@ -1,22 +1,23 @@
 import numpy as np
 import scipy.io as sio
 from timeit import default_timer as timer
-from scipy.sparse import csr_matrix
 
 def conjgrad(A, b, tol, x):
-    n = len(A)
-    r = b - np.dot(A, x)
+    n = len(b)
+    r = b - A.dot(x)
+    #r = b - np.dot(A, x)
     p = r
     rsold = np.dot(r.T, r)
     for i in range(n):
-        Ap = np.dot(A, p)
+        Ap = A.dot(p)
+        #Ap = np.dot(A, p)
         alpha = rsold / np.dot(p.T, Ap)
         x = x + alpha * p
         r = r - alpha * Ap
         rsnew = np.dot(r.T, r)
         if np.sqrt(rsnew) < tol:
             print('Itr:', i)
-            print(x)
+            #print(x)
             break
         p = r + (rsnew / rsold) * p
         rsold = rsnew
@@ -27,17 +28,13 @@ if __name__ == '__main__':
     data2 = sio.loadmat('./data/100x100/f_forceboundary_nodes100x100.mat')
     data3 = sio.loadmat('./data/100x100/x0_nodes100x100.mat')
     A = data1['K_Forceboundary_nodes100x100']
-    A = A.toarray()
+    #AA = A.toarray()
     b = data2['f_forceboundary_nodes100x100']
     x = data3['x0_nodes100x100']
 
-    # Toy Test Matrix A = 2x2
-    #A = np.array([[4., 1.],[1., 3.]])
-    #b = np.array([[1.], [2.]])
-
     # Toy Test Matrix A = 8x8
-    bb = np.array([[1], [1], [1], [1], [1], [1], [1], [1]])
-    AA = np.array([[6, 0, 1, 2, 0, 0, 2, 1],
+    b_test = np.array([[1], [1], [1], [1], [1], [1], [1], [1]])
+    A_test = np.array([[6, 0, 1, 2, 0, 0, 2, 1],
                   [0, 5, 1, 1, 0, 0, 3, 0],
                   [1, 1, 6, 1, 2, 0, 1, 2],
                   [2, 1, 1, 7, 1, 2, 1, 1],
@@ -45,16 +42,23 @@ if __name__ == '__main__':
                   [0, 0, 0, 2, 0, 4, 1, 0],
                   [2, 3, 1, 1, 2, 1, 5, 1],
                   [1, 0, 2, 1, 1, 0, 1, 3]])
-    #x = np.array([[0], [0], [0], [0], [0], [0], [0], [0]])
+    x_test = np.array([[0], [0], [0], [0], [0], [0], [0], [0]])
+
+    # Tolerance: Decrease for grater accuracy
     tol = 1e-5
 
-    start = timer()
+    start_py = timer()
 
-    x = conjgrad(A, b, tol, x)
+    x_result_py = conjgrad(A, b, tol, x)
 
-    end = timer()
-    print(end - start) # Time in seconds
+    end_py = timer()
 
-    # x_test = np.linalg.solve(A, b)
+    print('Python solved in ',  end_py - start_py, ' Seconds.')
+
+
+    # Test Solutions
+
+    #x_test = conjgrad(A, b, tol, x_test)
+    #x_linalg = np.linalg.solve(AA, b)
 
 
